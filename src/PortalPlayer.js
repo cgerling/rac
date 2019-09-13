8708e8788c71454b9098c05070794ae49f0c5b70
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-13 20:09:36 -0300

    refactor(PortalPlayer): allow external styling through classes injection

--- src/PortalPlayer.js
+++ src/PortalPlayer.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import cn from './utils/classNames'
 import './PortalPlayer.css'
 
 const Portal = ({
+  className,
   src,
 }) => {
   return (
-    <div className="PortalPlayer">
+    <div className={cn('PortalPlayer', className)}>
       <video src={src} />
     </div>
   )
