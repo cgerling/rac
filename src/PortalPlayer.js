8ba30368d264d1e2f2cbb20db76de2de2c46cc1e
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-13 21:09:15 -0300

    feat(PortalPlayer): create refs for both container and video elements

--- src/PortalPlayer.js
+++ src/PortalPlayer.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import cn from './utils/classNames'
+
 import './PortalPlayer.css'
 
 const Portal = ({
   className,
   src,
 }) => {
+  const playerRef = useRef()
+  const videoRef = useRef()
+
   return (
-    <div className={cn('PortalPlayer', className)}>
-      <video className="PortalPlayer__video" src={src} />
+    <div 
+      className={cn('PortalPlayer', className)}
+      ref={playerRef}
+    >
+      <video 
+        className="PortalPlayer__video" 
+        ref={videoRef} 
+        src={src} 
+      />
     </div>
   )
 }
