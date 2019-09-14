557528ae0bbd77cc3eaa12b8dd0d095787acaaee
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-14 15:07:34 -0300

    refactor(ToggleButton): change prop name from initial to value

--- src/components/ToggleButton/ToggleButton.js
+++ src/components/ToggleButton/ToggleButton.js
@@ -6,21 +6,21 @@ const ToggleButton = ({
   children, 
   className,
   onChange = () => {},
-  initial = false,
+  value = false,
   ...props
 }) => {
-  if (typeof initial === 'function') {
-    initial = initial()
+  if (typeof value === 'function') {
+    value = value()
   }
-  const [active, setActive] = useState(Boolean(initial))
+  const [active, setActive] = useState(Boolean(value))
 
   return (
     <Button
       className={cn('ToggleButton', className)}
       onClick={() => {
-        const value = !active
-        setActive(value)
-        onChange(value)
+        const state = !active
+        setActive(state)
+        onChange(state)
       }}
       {...props}
     >
