82d615afd0cea59d6bb0108d1ca53fd02d6eef11
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-14 16:51:46 -0300

    refactor(Overlay): add inner container to avoid unecessary re-renders

--- src/components/Overlay/Overlay.js
+++ src/components/Overlay/Overlay.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react'
-import { cn } from '../../utils/classNames'
+import cn from '../../utils/classNames'
 import { evaluate } from '../../utils/props'
 import './Overlay.css'
 
 const Overlay = ({ 
   className,
   children,
-  initial = false
+  initial = false,
 }) => {
   initial = evaluate(initial)
   const [active, setActive] = useState(Boolean(initial))
 
   return (
     <div 
-      className={cn('Overlay', className)}
+      className="Overlay"
       onPointerEnter={() => setActive(true)}
       onPointerLeave={() => setActive(false)}
     >
-      {active && children}
+      <div className={cn('Overlay__container', { active }, className)}>
+        {children}
+      </div>
     </div>
   )
 }
