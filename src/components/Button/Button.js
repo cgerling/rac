a3839466978008e3ff9454a755c2fd2533691a2b
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-14 10:11:48 -0300

    feat(Button): add prop that configure the initial component state
    Control the :hover state through React with internal state and allow it to its initial state be configurable

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import cn from '../../utils/classNames'
 import './Button.css'
 
-const Button = ({ 
+const Button = ({
+  active = false,
   children, 
   className,
+  onPointerEnter = () => {},
+  onPointerLeave = () => {},
   ...props
 }) => {
+  if (typeof active === 'function') {
+    active = active()
+  }
+  const [activated, setActivated] = useState(active)
+
   return (
     <button 
-      className={cn('Button', className)}
+      className={cn('Button', activated && 'Button--active', className)}
+      onPointerEnter={(e) => {
+        setActivated(true)
+        onPointerEnter(e)
+      }}
+      onPointerLeave={(e) => {
+        setActivated(false)
+        onPointerLeave(e)
+      }}
       {...props}
     >
       {children}
