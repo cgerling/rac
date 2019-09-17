7c79c784b637c449ba47a3d732a02505fd6ce31d
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-17 20:33:54 -0300

    refactor(PortalPlayer): organize and position properly the player controls

--- src/PortalPlayer.js
+++ src/PortalPlayer.js
@@ -39,80 +39,84 @@ const Portal = ({
     >
       <video 
         className="PortalPlayer__video"
-        controls
         ref={videoRef} 
         src={src} 
       />
-      <Overlay className="PortalPlayer__controls">
-        <Volume
-          className="PortalPlayer__volume"
-          onChange={(level) => {
-            const volume = level / 100
-            videoRef.current.volume = volume
-            videoRef.current.muted = volume === 0
-          }}
-        />
-        <Selector 
-          className="PortalPlayer__subtitles-selector"
-          options={options} 
-          onChange={(value) => setSubtitle(value)}
-        >
-          <MessageSquare />
-        </Selector>
-        <Button
-          className="PortalPlayer__backward-time"
-          onClick={() => {
-            videoRef.current.currentTime -= 5
-          }}
-        >
-          <ChevronsLeft />
-        </Button>
-        <Button
-          className="PortalPlayer__forward-time"
-          onClick={() => {
-            videoRef.current.currentTime += 5
-          }}
-        >
-          <ChevronsRight />
-        </Button>
-        <ToggleButton
-          className="PortalPlayer__playback-button"
-          value={() => videoRef.current && !videoRef.current.paused}
-          onChange={(active) => {
-            if (!active) {
-              return videoRef.current.pause()
-            }
-            
-            return videoRef.current.play().then(console.info.bind(console), console.error.bind(console))
-          }}
-        >
-          {(active) => {
-            if (!active) {
-              return <Play />
-            }
+      <Overlay>
+        <div className="PortalPlayer__playback">
+          <Button
+            className="PortalPlayer__backward-time"
+            onClick={() => {
+              videoRef.current.currentTime -= 5
+            }}
+          >
+            <ChevronsLeft />
+          </Button>
+          <ToggleButton
+            className="PortalPlayer__playback-button"
+            value={() => videoRef.current && !videoRef.current.paused}
+            onChange={(active) => {
+              if (!active) {
+                return videoRef.current.pause()
+              }
+              
+              return videoRef.current.play().then(console.info.bind(console), console.error.bind(console))
+            }}
+          >
+            {(active) => {
+              const size = 48
+              if (!active) {
+                return <Play fill="currentColor" size={size} />
+              }
 
-            return <Pause />
-          }}
-        </ToggleButton>
-        <ToggleButton
-          className="PortalPlayer__screen-toggle"
-          initial={document.fullscreenElement}
-          onChange={(active) => {
-            if (!active) {
-              return document.exitFullscreen()
-            }
-            
-            return playerRef.current.requestFullscreen({ navigationUI: 'hide' })
-          }}
-        >
-          {(active) => {
-            if (!active) {
-              return <Maximize />
-            }
+              return <Pause fill="currentColor" size={size} />
+            }}
+          </ToggleButton>
+          <Button
+            className="PortalPlayer__forward-time"
+            onClick={() => {
+              videoRef.current.currentTime += 5
+            }}
+          >
+            <ChevronsRight />
+          </Button>
+        </div>
+        <div className="PortalPlayer__controls">
+          <Selector 
+            className="PortalPlayer__subtitles-selector"
+            options={options} 
+            onChange={(value) => setSubtitle(value)}
+          >
+            <MessageSquare />
+          </Selector>
+          <Volume
+            className="PortalPlayer__volume"
+            onChange={(level) => {
+              const volume = level / 100
+              videoRef.current.volume = volume
+              videoRef.current.muted = volume === 0
+            }}
+          />
+          <ToggleButton
+            className="PortalPlayer__screen-toggle"
+            initial={document.fullscreenElement}
+            onChange={(active) => {
+              if (!active) {
+                return document.exitFullscreen()
+              }
+              
+              return playerRef.current.requestFullscreen({ navigationUI: 'hide' })
+            }}
+          >
+            {(active) => {
+              if (!active) {
+                return <Maximize />
+              }
 
-            return <Minimize />
-          }}
-        </ToggleButton>
+              return <Minimize />
+            }}
+          </ToggleButton>
+        </div>
       </Overlay>
     </div>
   )
