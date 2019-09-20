e3b66758436d2a63b518f92c8b3b24310d5ea001
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-20 16:37:44 -0300

    feat(FontAwesome): create custom library with all needed icons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PortalPlayer from './PortalPlayer'
+
+import './FontAwesome'
 import './App.css';
 
 function App() {
---
+++ src/FontAwesome.js
@@ -0,0 +1,17 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import * as icon from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  icon.faPlay, 
+  icon.faPause,
+  icon.faExpand,
+  icon.faCompress,
+  icon.faCommentAlt,
+  icon.faUndoAlt,
+  icon.faRedoAlt,
+  icon.faVolumeMute,
+  icon.faVolumeOff,
+  icon.faVolumeDown,
+  icon.faVolumeUp,
+  icon.faCheck,
+)
