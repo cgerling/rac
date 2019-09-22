8fbab3031b3c7863e08be050c315ee698ad9a376
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-22 15:32:05 -0300

    feat(Loader): add component to represent loading state

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import cn from '../../utils/classNames'
+
+import './Loader.css'
+
+const Loader = ({
+  className,
+}) => {
+  return (
+    <div className={cn('Loader', {}, className)}>
+      <svg
+        className="Loader__container"
+        viewBox="25 25 50 50"
+      >
+        <circle
+          className="Loader__circle"
+          cx="50"
+          cy="50"
+          r="20"
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default Loader
