9cc23a9fb5909986a0f9d893ff898fb4996faaa2
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-29 18:45:23 -0300

    build(rollup): add css plugins and uglify

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import postcss from 'rollup-plugin-postcss'
 import cssModules from 'postcss-modules'
+import autoprefixer from 'autoprefixer'
+import uglify from 'rollup-plugin-uglify'
 
 const cssExportMap = {}
 
@@ -10,7 +12,8 @@ export default {
   input: 'src/index.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'cjs'
+    format: 'cjs',
+    sourcemap: true,
   },
   external: [
     '@fortawesome/fontawesome-svg-core',
@@ -25,21 +28,23 @@ export default {
     resolve(),
     postcss({
       plugins: [
+        autoprefixer(),
         cssModules({
-          getJSON (id, exportTokens) {
+          getJSON(id, exportTokens) {
             cssExportMap[id] = exportTokens
           },
           getExportedName: false,
-          getExport (id) {
+          getExport(id) {
             return cssExportMap[id]
           },
-          extract: 'dist/styles.css'
-        })
-      ]
+          extract: 'dist/styles.css',
+        }),
+      ],
     }),
     babel({
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
     }),
     commonjs(),
-  ]
+    uglify,
+  ],
 }
