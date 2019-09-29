2894c13c960c64a1402b1b82d5c128fef9e4be1b
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-29 15:43:07 -0300

    style(eslint): format with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:jsx-a11y/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "jsx-a11y"
-    ],
-    "rules": {
-        "react/prop-types": "warn"
-    }
-};
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jsx-a11y'],
+  rules: {
+    'react/prop-types': 'warn',
+  },
+}
