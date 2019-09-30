2b41e19832de975d500984b7dd507090c0a512e2
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-29 21:50:18 -0300

    feat(Icon): create component to encapsulate FontAwesome configuration

---
+++ src/components/Icon/Icon.js
@@ -0,0 +1,19 @@
+import {library} from '@fortawesome/fontawesome-svg-core'
+import * as icon from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  icon.faPlay,
+  icon.faPause,
+  icon.faExpand,
+  icon.faCompress,
+  icon.faCommentAlt,
+  icon.faUndoAlt,
+  icon.faRedoAlt,
+  icon.faVolumeMute,
+  icon.faVolumeOff,
+  icon.faVolumeDown,
+  icon.faVolumeUp,
+  icon.faCheck,
+)
+
+export {FontAwesomeIcon as default} from '@fortawesome/react-fontawesome'
