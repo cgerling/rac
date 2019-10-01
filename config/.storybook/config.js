6bdd688246acc12d6ed1a7832b2b36ee74f9f8b6
Calvin Gerling <calvin.gerling@singlebit.org>
2019-09-30 22:18:38 -0300

    refactor(storybook): put configuration files into a config/ folder

--- config/.storybook/config.js
+++ config/.storybook/config.js
@@ -1,6 +1,6 @@
 import {configure} from '@storybook/react'
 
-const req = require.context('../stories', true, /.stories.js$/)
+const req = require.context('../../stories', true, /.stories.js$/)
 function loadStories() {
   req.keys().forEach(req)
 }
